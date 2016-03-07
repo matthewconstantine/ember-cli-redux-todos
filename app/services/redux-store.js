2760d5f624359d0c7118eaa50eb0304c355365db
Matthew Constantine <designmc@gmail.com>
2016-03-07 12:45:46 -0800

    Add a configurable reduxStore.

--- app/services/redux-store.js
+++ app/services/redux-store.js
@@ -1,13 +1,11 @@
-import Ember from 'ember';
 import ReduxStore from 'ember-cli-redux/services/redux-store';
 import reducer from '../reducers/index';
+import emberLoggerMiddleware from 'ember-cli-redux/lib/ember-logger-middleware';
+
+const logger = emberLoggerMiddleware();
 
 export default ReduxStore.extend({
   reducer,
 
-  dispatch(action) {
-    console.log('hey! ', action)
-    this._super.apply(this, arguments);
-  }
+  middleware: [logger],
 });
-
