09443657041515b9588de2a3b8e12d5d37f241e3
Matthew Constantine <designmc@gmail.com>
2016-03-11 16:42:42 -0800

    Upgrade to ember-cli-redux 0.2.0 and disable logging on prod.

--- app/services/redux-store.js
+++ app/services/redux-store.js
@@ -1,8 +1,11 @@
 import ReduxStore from 'ember-cli-redux/services/redux-store';
 import reducer from '../reducers/index';
 import emberLoggerMiddleware from 'ember-cli-redux/lib/ember-logger-middleware';
+import config from '../config/environment';
 
-const logger = emberLoggerMiddleware();
+const logger = emberLoggerMiddleware({
+  enabled: config.environment === 'dev'
+});
 
 export default ReduxStore.extend({
   reducer,
